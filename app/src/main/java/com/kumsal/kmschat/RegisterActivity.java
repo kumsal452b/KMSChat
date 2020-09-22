5a7eb79f487ecaf98756aa48a37fc9326e5db0e4
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-22 15:29:39 +0300

    Nothing

--- app/src/main/java/com/kumsal/kmschat/RegisterActivity.java
+++ app/src/main/java/com/kumsal/kmschat/RegisterActivity.java
@@ -1,14 +1,69 @@
 package com.kumsal.kmschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplay,mEmail,password;
+    private Button regBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mDisplay=findViewById(R.id.reg_display);
+        mEmail=findViewById(R.id.reg_email);
+        password=findViewById(R.id.reg_password);
+        regBtn=findViewById(R.id.reg_create_account);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String display=mDisplay.getEditText().getText().toString();
+                String email=mEmail.getEditText().getText().toString();
+                String password1=password.getEditText().getText().toString();
+                register_user(display,email,password1);
+            }
+        });
+    }
+
+    private void register_user(String display, String email, String password1) {
+        mAuth.createUserWithEmailAndPassword(email, password1)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent myIntent=new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(myIntent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+
+
+
     }
 }
