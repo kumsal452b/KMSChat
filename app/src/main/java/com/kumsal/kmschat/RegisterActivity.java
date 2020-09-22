197bc2143f3f0e94d21ceaf778cbe18b99e52ad3
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-22 16:27:19 +0300

    fix it the firebase authetication

--- app/src/main/java/com/kumsal/kmschat/RegisterActivity.java
+++ app/src/main/java/com/kumsal/kmschat/RegisterActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
@@ -31,6 +33,7 @@ public class RegisterActivity extends AppCompatActivity {
         mEmail=findViewById(R.id.reg_email);
         password=findViewById(R.id.reg_password);
         regBtn=findViewById(R.id.reg_create_account);
+        mAuth=FirebaseAuth.getInstance();
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,18 +50,20 @@ public class RegisterActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Intent myIntent=new Intent(getApplicationContext(),MainActivity.class);
-                            startActivity(myIntent);
-                            finish();
+                        task.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(myIntent);
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegisterActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-
-                        // ...
                     }
                 });
 
