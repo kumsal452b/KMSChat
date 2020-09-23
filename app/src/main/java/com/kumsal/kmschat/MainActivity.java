4bdaaef22b1ea015d142c37c986c2826f89b923c
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-23 14:10:07 +0300

    added appbar

--- app/src/main/java/com/kumsal/kmschat/MainActivity.java
+++ app/src/main/java/com/kumsal/kmschat/MainActivity.java
@@ -1,9 +1,12 @@
 package com.kumsal.kmschat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,13 +14,17 @@ import com.jfoenix.controls.JFXButton;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
+    private Toolbar mToolbar;
     JFXButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth=FirebaseAuth.getInstance();
+        mToolbar=findViewById(R.id.main_app_bar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("KMSChat");
 
     }
 
