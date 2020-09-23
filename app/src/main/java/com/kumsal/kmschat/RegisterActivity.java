938d9eaf2350bdfd93619de0cffb7d07cf5cff78
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-23 17:04:42 +0300

    added a new prop

--- app/src/main/java/com/kumsal/kmschat/RegisterActivity.java
+++ app/src/main/java/com/kumsal/kmschat/RegisterActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,7 +32,7 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Toolbar  toolbar;
     private ActionBarDrawerToggle togle;
-
+    private ProgressDialog mProgresDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +42,8 @@ public class RegisterActivity extends AppCompatActivity {
         password=findViewById(R.id.reg_password);
         regBtn=findViewById(R.id.reg_create_account);
         toolbar=findViewById(R.id.register_toolbar);
+        mProgresDialog=new ProgressDialog(this);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Create Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -50,7 +55,13 @@ public class RegisterActivity extends AppCompatActivity {
                 String display=mDisplay.getEditText().getText().toString();
                 String email=mEmail.getEditText().getText().toString();
                 String password1=password.getEditText().getText().toString();
-                register_user(display,email,password1);
+                if (!TextUtils.isEmpty(display) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password1)){
+                    mProgresDialog.setTitle("Registering User");
+                    mProgresDialog.setMessage("Please wait while we create your account");
+                    mProgresDialog.setCanceledOnTouchOutside(false);
+                    mProgresDialog.show();
+                    register_user(display,email,password1);
+                }
             }
         });
     }
@@ -62,6 +73,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void register_user(String display, String email, String password1) {
+
         mAuth.createUserWithEmailAndPassword(email, password1)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -69,6 +81,7 @@ public class RegisterActivity extends AppCompatActivity {
                         task.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                             @Override
                             public void onSuccess(AuthResult authResult) {
+                                mProgresDialog.dismiss();
                                 Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
                                 startActivity(myIntent);
                                 finish();
