f38670cd30d0a44fb24fc46f4483d54d662642dd
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-24 21:26:32 +0300

    end of day

--- app/src/main/java/com/kumsal/kmschat/SectionPagerAdapter.java
+++ app/src/main/java/com/kumsal/kmschat/SectionPagerAdapter.java
@@ -17,9 +17,11 @@ public class SectionPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
+
                 RequestFragment requestFragment = new RequestFragment();
                 return requestFragment;
             case 1:
+
                 ChatFragments chatFragments = new ChatFragments();
                 return chatFragments;
             case 2:
@@ -33,5 +35,18 @@ public class SectionPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 3;
+    }
+    public CharSequence getPageTitle(int position){
+        switch (position) {
+            case 0:
+                return "REQUEST";
+            case 1:
+                return "CHAT";
+            case 2:
+                return "FRIENDS";
+            default:
+                return null;
+        }
+
     }
 }
