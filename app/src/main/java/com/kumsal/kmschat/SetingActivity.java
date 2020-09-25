9ae130273b24cd5b6b124c6ac48d9af33b6f4830
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-25 21:31:16 +0300

    end of day

--- app/src/main/java/com/kumsal/kmschat/SetingActivity.java
+++ app/src/main/java/com/kumsal/kmschat/SetingActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,24 +16,42 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SetingActivity extends AppCompatActivity {
 
 
     private DatabaseReference mRefDatabase;
     private FirebaseUser mUser;
+    private TextView name,status;
+    private CircleImageView imageView;
+    private Button changestatus;
+    private Button changeimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setings);
         mUser= FirebaseAuth.getInstance().getCurrentUser();
         String curentID=mUser.getUid();
+        name=findViewById(R.id.seting_displayname);
+        status=findViewById(R.id.seting_hi);
+        imageView=findViewById(R.id.setings_circleimage);
+        changeimage=findViewById(R.id.seting_statusimage);
+        changestatus=findViewById(R.id.seting_statuschanges);
+
         mRefDatabase= FirebaseDatabase.getInstance().getReference("Users").child(curentID);
 
         mRefDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name1=snapshot.child("name").getValue().toString();
+                String image1=snapshot.child("imagesUrl").getValue().toString();
+                String status1=snapshot.child("status").getValue().toString();
+                String thumball1=snapshot.child("thumbalimage").getValue().toString();
+                name.setText(name1);
+                status.setText(status1);
 
-                Toast.makeText(SetingActivity.this, snapshot.toString(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
