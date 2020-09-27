ea1de953bf333aa82a8d5ce35cd8f6dc2792ea27
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-27 21:16:42 +0300

    falied day

--- app/src/main/java/com/kumsal/kmschat/UserActıvıty.java
+++ app/src/main/java/com/kumsal/kmschat/UserActıvıty.java
@@ -1,15 +1,24 @@
 package com.kumsal.kmschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserActıvıty extends AppCompatActivity {
     private Toolbar toolbar;
     private RecyclerView recyclerView;
+    private DatabaseReference mrefDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +29,37 @@ public class UserActıvıty extends AppCompatActivity {
         recyclerView=findViewById(R.id.user_recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mrefDatabase= FirebaseDatabase.getInstance().getReference("Users");
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        
+        FirebaseRecyclerOptions<Users> options=new FirebaseRecyclerOptions.Builder<Users>()
+                .build();
+        FirebaseRecyclerAdapter<Users,UsersViewHolder> firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<Users, UsersViewHolder>(
+                Users.class,
+                R.layout.user_single,
+                UsersViewHolder.class
+        ) {
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
+
+            }
+
+            @NonNull
+            @Override
+            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+        };
+    }
+
+    public class  UsersViewHolder extends RecyclerView.ViewHolder{
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
