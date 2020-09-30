15fc2c8e1aee8069649d6876c430b253c4547c40
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-09-30 19:52:45 +0300

    new uptadet ava''lable

--- app/src/main/java/com/kumsal/kmschat/SetingActivity.java
+++ app/src/main/java/com/kumsal/kmschat/SetingActivity.java
@@ -189,8 +189,15 @@ public class SetingActivity extends AppCompatActivity {
                 String filename = "profile_image"+ FirebaseAuth.getInstance().getCurrentUser().getUid();
                 final StorageReference filePath = imageStorage.getReference().child("profile_images")
                         .child(filename+".jpeg");
-                File file=new
-                storageTask = filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+               OutputStream os2=new ByteArrayOutputStream();
+               resultbitmap.compress(Bitmap.CompressFormat.JPEG,75,os2);
+               byte[] imageresult=new byte[];
+                try {
+                    os2.write(imageresult);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                storageTask = filePath.putBytes(imageresult).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                     if (task.isSuccessful()){
