ad89cbfa76776d890c3b6e6ff33bd9df7335d0f0
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-01 20:48:40 +0300

    add new xml line

--- app/src/main/java/com/kumsal/kmschat/ProfileActivity.java
+++ app/src/main/java/com/kumsal/kmschat/ProfileActivity.java
@@ -3,12 +3,17 @@ package com.kumsal.kmschat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private TextView profile_display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profile_display=findViewById(R.id.profile_displayname);
+        profile_display.setText(getIntent().getStringExtra("ui"));
     }
 }
