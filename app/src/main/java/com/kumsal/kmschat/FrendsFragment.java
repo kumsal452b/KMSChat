ac939bb8b7d84e8b685e855c834222276ef48e46
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-10 19:42:32 +0300

    noting

--- app/src/main/java/com/kumsal/kmschat/FrendsFragment.java
+++ app/src/main/java/com/kumsal/kmschat/FrendsFragment.java
@@ -8,15 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FrendsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FrendsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,14 +20,6 @@ public class FrendsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FrendsFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static FrendsFragment newInstance(String param1, String param2) {
         FrendsFragment fragment = new FrendsFragment();
@@ -58,7 +42,6 @@ public class FrendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_frends, container, false);
     }
 }
