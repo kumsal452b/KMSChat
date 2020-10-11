bdebc2d72f87e7f9e892a8ffe993e46f37f8855e
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-11 23:19:40 +0300

    Add new elemet

--- app/src/main/java/com/kumsal/kmschat/FrendsFragment.java
+++ app/src/main/java/com/kumsal/kmschat/FrendsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -18,12 +19,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FrendsFragment extends Fragment {
     private View mMainView;
     private String mCurrent_user_id;
     private FirebaseAuth mAuth;
     private DatabaseReference mFriendsDatabase;
     private RecyclerView mFriendList;
+    private List<FriendModdel> friendModdels;
+    private FriendsAdapter adapter;
     public FrendsFragment() {
         // Required empty public constructor
     }
@@ -35,13 +41,16 @@ public class FrendsFragment extends Fragment {
         mAuth=FirebaseAuth.getInstance();
         FirebaseUser user=mAuth.getCurrentUser();
         mCurrent_user_id=user.getUid();
+        friendModdels=new ArrayList<>();
         mFriendsDatabase= FirebaseDatabase.getInstance().getReference().child("friendList").child(mCurrent_user_id);
         mFriendList=mMainView.findViewById(R.id.friendFragmentsRecycler);
+        mFriendList.setLayoutManager(new LinearLayoutManager(getContext()));
+        mFriendList.setHasFixedSize(true);
         mFriendsDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot get: snapshot.getValue()){
-                    
+                for (DataSnapshot get: snapshot.getChildren()){
+                        
                 }
             }
 
