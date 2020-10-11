98973ce924a45f455e650f5940ac423c078e66ef
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-11 22:39:46 +0300

    Nothıng

--- app/src/main/java/com/kumsal/kmschat/FriendsAdapter.java
+++ app/src/main/java/com/kumsal/kmschat/FriendsAdapter.java
@@ -10,6 +10,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -17,7 +25,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.friendsViewHolder> {
     private List<FriendModdel> moddelList;
     private Context context;
-    private  
+
+
     @NonNull
     @Override
     public friendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -26,8 +35,23 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.friendsV
     }
 
     @Override
-    public void onBindViewHolder(@NonNull friendsViewHolder holder, int position) {
-        FriendModdel moddel=moddelList.get(position);
+    public void onBindViewHolder(@NonNull final friendsViewHolder holder, int position) {
+        final FriendModdel moddel=moddelList.get(position);
+
+        holder.mRef.child(moddel.getmKey()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                holder.date.setText(moddel.getDate());
+                HashMap<String,String> values=(HashMap<String,String>)snapshot.getValue();
+                holder.displayName.setText(values.get("name"));
+                Picasso.get().load(values.get("imagesUrl")).into(holder.imageView);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
@@ -38,14 +62,17 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.friendsV
     }
 
     class friendsViewHolder extends RecyclerView.ViewHolder{
-        TextView date,displayName;
-        CircleImageView imageView;
+        public TextView date,displayName;
+        public CircleImageView imageView;
+        public DatabaseReference mRef;
         public friendsViewHolder(@NonNull View itemView) {
             super(itemView);
 
             date=itemView.findViewById(R.id.users_single_status);
             displayName=itemView.findViewById(R.id.user_single_name);
             imageView=itemView.findViewById(R.id.user_single_imageview);
+
+            mRef= FirebaseDatabase.getInstance().getReference().child("Users");
 
         }
     }
