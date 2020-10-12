749ebc24549037c367f4fb4bdc6b36e80c1e0eb7
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-12 21:12:01 +0300

    add setter and getter for moddel  class

--- app/src/main/java/com/kumsal/kmschat/FriendModdel.java
+++ app/src/main/java/com/kumsal/kmschat/FriendModdel.java
@@ -4,5 +4,35 @@ package com.kumsal.kmschat;
 public class FriendModdel{
     private String Date;
     private String mDisplayName;
+    private String imageUrl;
 
+    public FriendModdel(String date, String mDisplayName, String imageUrl) {
+        Date = date;
+        this.mDisplayName = mDisplayName;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getmDisplayName() {
+        return mDisplayName;
+    }
+
+    public void setmDisplayName(String mDisplayName) {
+        this.mDisplayName = mDisplayName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
