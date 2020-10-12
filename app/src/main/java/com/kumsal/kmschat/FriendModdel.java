9a3f36be7a169c8613d9a946b391ab9099bc7f9a
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-12 21:01:57 +0300

    modıfy the class

--- app/src/main/java/com/kumsal/kmschat/FriendModdel.java
+++ app/src/main/java/com/kumsal/kmschat/FriendModdel.java
@@ -3,26 +3,6 @@ package com.kumsal.kmschat;
 
 public class FriendModdel{
     private String Date;
-    private String mKey;
+    private String mDisplayName;
 
-    public FriendModdel(String date, String mKey) {
-        Date = date;
-        this.mKey = mKey;
-    }
-
-    public String getDate() {
-        return Date;
-    }
-
-    public void setDate(String date) {
-        Date = date;
-    }
-
-    public String getmKey() {
-        return mKey;
-    }
-
-    public void setmKey(String mKey) {
-        this.mKey = mKey;
-    }
 }
