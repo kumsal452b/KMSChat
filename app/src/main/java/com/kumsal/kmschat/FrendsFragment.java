9539eb85616a37ce9fec8dc4b4410441b534ed32
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-14 11:26:30 +0300

    New lıne added for the program is runing

--- app/src/main/java/com/kumsal/kmschat/FrendsFragment.java
+++ app/src/main/java/com/kumsal/kmschat/FrendsFragment.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +21,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.kongzue.dialog.util.view.ProgressView;
+import com.kongzue.dialog.v3.WaitDialog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,7 +48,40 @@ public class FrendsFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        System.out.println("run onStart");
+        System.out.println("Activite "+ getActivity());
+        mFriendsDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot get: snapshot.getChildren()){
+                    values=( HashMap<String,String>)get.getValue();
+                    mRef.child(get.getKey()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                            values2=( HashMap<String,String>)snapshot.getValue();
+//                            object=new FriendModdel(values.get("date"),values2.get("name"),values2.get("thumbalimage"));
+//                            friendModdels.add(object);
+//
+//                            adapter=new FriendsAdapter(friendModdels,getContext());
+//                            mFriendList.setAdapter(adapter);
+//                            adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
@@ -60,38 +98,9 @@ public class FrendsFragment extends Fragment {
         mFriendList=mMainView.findViewById(R.id.friendFragmentsRecycler);
         mFriendList.setLayoutManager(new LinearLayoutManager(getContext()));
         mFriendList.setHasFixedSize(true);
+
         mRef = FirebaseDatabase.getInstance().getReference().child("Users");
-        mFriendsDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot get: snapshot.getChildren()){
-                    values=( HashMap<String,String>)get.getValue();
-                    mRef.child(get.getKey()).addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            values2=( HashMap<String,String>)snapshot.getValue();
-                            object=new FriendModdel(values.get("date"),values2.get("name"),values2.get("thumbalimage"));
-                            friendModdels.add(object);
 
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-
-                }
-                adapter=new FriendsAdapter(friendModdels,getContext());
-                mFriendList.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        return inflater.inflate(R.layout.fragment_frends, container, false);
+        return mMainView;
     }
 }
