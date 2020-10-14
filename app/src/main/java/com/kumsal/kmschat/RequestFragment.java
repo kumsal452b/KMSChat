e7d8f6a3a755921613835dce9e711470df587bce
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-14 11:33:18 +0300

    Added new lıb

--- app/src/main/java/com/kumsal/kmschat/RequestFragment.java
+++ app/src/main/java/com/kumsal/kmschat/RequestFragment.java
@@ -2,6 +2,7 @@ package com.kumsal.kmschat;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,8 @@ import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.kongzue.dialog.v3.WaitDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +35,7 @@ public class RequestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        WaitDialog.show((AppCompatActivity) getContext(),"selam ");
         View view=inflater.inflate(R.layout.fragment_request, container, false);
         ListView listView=view.findViewById(R.id.deneme);
         List<String> deney=new ArrayList<>();
@@ -39,6 +43,7 @@ public class RequestFragment extends Fragment {
         deney.add("yahya");
         ListAdapter adapter=new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,deney);
         listView.setAdapter(adapter);
+        WaitDialog.dismiss();
         return view;
     }
 }
