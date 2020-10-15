b1ef2533a196a9ec28747f5426276d16f1846380
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-15 13:22:57 +0300

    Deleted

--- app/src/main/java/com/kumsal/kmschat/RequestFragment.java
+++ app/src/main/java/com/kumsal/kmschat/RequestFragment.java
@@ -35,12 +35,7 @@ public class RequestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        WaitDialog.show((AppCompatActivity) getContext(),"selam ");
-        View view=inflater.inflate(R.layout.fragment_request, container, false);
-        ListView listView=view.findViewById(R.id.deneme);
-        List<String> deney=new ArrayList<>();
-        deney.add("deney");
-        deney.add("yahya");
+       
         ListAdapter adapter=new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,deney);
         listView.setAdapter(adapter);
         WaitDialog.dismiss();
