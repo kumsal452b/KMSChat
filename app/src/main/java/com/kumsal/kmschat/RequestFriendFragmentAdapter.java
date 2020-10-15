ddae75fd421ef7b257cfe698abec3435507b007f
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-15 14:36:34 +0300

    fınıshed adapter

--- app/src/main/java/com/kumsal/kmschat/RequestFriendFragmentAdapter.java
+++ app/src/main/java/com/kumsal/kmschat/RequestFriendFragmentAdapter.java
@@ -2,15 +2,19 @@ package com.kumsal.kmschat;
 
 import android.content.Context;
 
+import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -19,8 +23,17 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class RequestFriendFragmentAdapter extends RecyclerView.Adapter<RequestFriendFragmentAdapter.FriendRequestHolder> {
 
     private Context context;
+
     private List<Users> userValue;
 
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener{
+         void ButtonClick(int position);
+    }
+    public void setOnItemClickListener(OnItemClickListener mListener){
+        this.mListener=mListener;
+    }
     public RequestFriendFragmentAdapter(Context context, List<Users> userValue) {
         this.context = context;
         this.userValue = userValue;
@@ -29,27 +42,44 @@ public class RequestFriendFragmentAdapter extends RecyclerView.Adapter<RequestFr
     @NonNull
     @Override
     public FriendRequestHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.request_friend,parent,false);
+        return new FriendRequestHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FriendRequestHolder holder, int position) {
-
+        Users person=userValue.get(position);
+        holder.status.setText(person.getStatus());
+        holder.dispayName.setText(person.getName());
+        Picasso.get().load(person.getImage()).into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return userValue.size();
     }
 
     public class FriendRequestHolder extends RecyclerView.ViewHolder{
         public CircleImageView imageView;
         public TextView dispayName,status;
+        public Button btnAccept;
         public FriendRequestHolder(@NonNull View itemView) {
             super(itemView);
             imageView=itemView.findViewById(R.id.request_friend_image);
             dispayName=itemView.findViewById(R.id.request_friend_displayName);
             status=itemView.findViewById(R.id.request_friend_status);
+            btnAccept=itemView.findViewById(R.id.request_friend_accept);
+            btnAccept.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mListener!=null){
+                        int positon=getAdapterPosition();
+                        if (positon!=RecyclerView.NO_POSITION){
+                            mListener.ButtonClick(positon);
+                        }
+                    }
+                }
+            });
         }
     }
 }
