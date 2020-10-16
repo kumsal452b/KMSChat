a2ed3c55df48e43163ef8b69753e0d1a27ec247e
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-16 18:14:30 +0300

    add new class element

--- app/src/main/java/com/kumsal/kmschat/KMSChat.java
+++ app/src/main/java/com/kumsal/kmschat/KMSChat.java
@@ -2,11 +2,20 @@ package com.kumsal.kmschat;
 
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 public class KMSChat extends Application {
+    private DatabaseReference mUserDb;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate() {
@@ -20,5 +29,21 @@ public class KMSChat extends Application {
         built.setIndicatorsEnabled(true);
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
+        mAuth=FirebaseAuth.getInstance();
+        mUserDb=FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getUid());
+        mUserDb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot!=null){
+                            mUserDb.child("online").onDisconnect().setValue("false");
+                            mUserDb.child("online").setValue("true");
+                        }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
