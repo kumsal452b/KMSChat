56b8679a94af1ae6e03e24c74a9d05b6d9375e6b
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-18 20:06:12 +0300

    Support the toolbar element

--- app/src/main/java/com/kumsal/kmschat/ChatActivity.java
+++ app/src/main/java/com/kumsal/kmschat/ChatActivity.java
@@ -1,15 +1,22 @@
 package com.kumsal.kmschat;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class ChatActivity extends AppCompatActivity {
-
+    private String UI;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
+        UI=getIntent().getStringExtra("ui");
+        toolbar=findViewById(R.id.chat_toolbar);
+        setActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
