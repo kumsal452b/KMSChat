3435213dd15a15f2d77995d11918238866723679
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-19 14:56:40 +0300

    add new dıaolog system

--- app/src/main/java/com/kumsal/kmschat/ChatActivity.java
+++ app/src/main/java/com/kumsal/kmschat/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.kumsal.kmschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,20 +8,42 @@ import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kongzue.dialog.v3.WaitDialog;
+
 public class ChatActivity extends AppCompatActivity {
     private String UI;
     private Toolbar toolbar;
     private ActionBarDrawerToggle toggle;
+    private DatabaseReference mRefUsers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         UI=getIntent().getStringExtra("ui");
-
+        mRefUsers= FirebaseDatabase.getInstance().getReference("Users");
         toolbar=findViewById(R.id.chat_toolbar);
         toolbar.setBackgroundColor(Color.GREEN);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Error");
+        WaitDialog.show(this,"Please Wait");
+        mRefUsers.child(UI).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name=snapshot.child("name").getValue().toString();
+                getSupportActionBar().setTitle(name);
+                WaitDialog.dismiss();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
