4f4d5de49dc6e936e604ee735e175946edf0b5d8
kumsal65 <70024440+kumsal67@users.noreply.github.com>
2020-10-19 15:42:46 +0300

    test is succes

--- app/src/main/java/com/kumsal/kmschat/ChatActivity.java
+++ app/src/main/java/com/kumsal/kmschat/ChatActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,12 +16,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.kongzue.dialog.v3.WaitDialog;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
     private String UI;
     private Toolbar toolbar;
     private ActionBarDrawerToggle toggle;
     private DatabaseReference mRefUsers;
+    private CircleImageView imageView;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +36,19 @@ public class ChatActivity extends AppCompatActivity {
         toolbar=findViewById(R.id.chat_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Error");
         WaitDialog.show(this,"Please Wait");
+        imageView=findViewById(R.id.circle_app_toolbar_dark);
+        textView=findViewById(R.id.textview_app_toolbar_dark);
         mRefUsers.child(UI).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String name=snapshot.child("name").getValue().toString();
-                getSupportActionBar().setTitle(name);
+                String image=snapshot.child("thumbalimage").getValue().toString();
+                if (image.equals("") || image.equals(null)){
+                    image="emty";
+                }
+                textView.setText(name);
+                Picasso.get().load(image).into(imageView);
                 WaitDialog.dismiss();
             }
 
