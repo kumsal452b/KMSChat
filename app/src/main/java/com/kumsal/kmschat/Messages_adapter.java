20bbb406363ac523bfbdc170a44aa0dbb7ff1293
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-24 18:26:45 +0300

    add new fındbyId element

--- app/src/main/java/com/kumsal/kmschat/Messages_adapter.java
+++ app/src/main/java/com/kumsal/kmschat/Messages_adapter.java
@@ -1,18 +1,31 @@
 package com.kumsal.kmschat;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Messages_adapter  extends RecyclerView.Adapter<Messages_adapter.message_holder> {
 
+
+    private List<Messages_Model> messages_modelList;
+
+    public Messages_adapter(List<Messages_Model> messages_modelList) {
+        this.messages_modelList = messages_modelList;
+    }
 
     @NonNull
     @Override
     public message_holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.message_single,parent,false);
+        return new message_holder(view);
     }
 
     @Override
@@ -22,13 +35,17 @@ public class Messages_adapter  extends RecyclerView.Adapter<Messages_adapter.mes
 
     @Override
     public int getItemCount() {
-        return 0; 
+        return messages_modelList.size();
     }
 
     public class message_holder extends RecyclerView.ViewHolder{
 
+        TextView message;
+        CircleImageView imageView;
         public message_holder(@NonNull View itemView) {
             super(itemView);
+            message=itemView.findViewById(R.id.message_text_layout);
+            imageView=itemView.findViewById(R.id.message_image_layout);
         }
     }
 }
