72411e82216fde2b33b70a9f46387085dd1e2005
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-25 16:42:49 +0300

    add new fıelds

--- app/src/main/java/com/kumsal/kmschat/Messages_Model.java
+++ app/src/main/java/com/kumsal/kmschat/Messages_Model.java
@@ -5,12 +5,14 @@ public class Messages_Model {
     private Long time;
     private Boolean seen;
     private String image;
-    public Messages_Model(String message, Long time, String type, Boolean seen,String image) {
+    private String from;
+    public Messages_Model(String message, Long time, String type, Boolean seen,String image,String from) {
         this.message = message;
         this.time = time;
         this.type = type;
         this.seen = seen;
         this.image=image;
+        this.from=from;
     }
 
     public String getMessage() {
@@ -52,5 +54,13 @@ public class Messages_Model {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
     }
 }
